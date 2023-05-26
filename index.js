8b8612b9114eb0e6730b4b0f6c187aa97bce7a17
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-26 17:35:23 +0530

    setup for deployment

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+app.use("/", (req, res) => {
+    return res.json({ success: true, message: "Successfully deployed." })
+})
+
 app.listen(PORT, () => {
     console.log("Server started at " + PORT);
 })
