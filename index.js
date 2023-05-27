e7d13e06934fba581f96c99ceed5a48b83ff065d
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-27 18:03:56 +0530

    / api default

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const Router = express.Router();
 const DbConnection = require("./DBConnection/connection");
 const UserRoutes = require("./routes/userRoutes")
 const SkillRoutes = require("./routes/SkillRoute");
@@ -11,13 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-// app.use("/", (req, res) => {
-//     return res.json({ success: true, message: "Successfully deployed." })
-// })
+
 
 app.use(cors({
     origin: '*'
 }));
+
+Router.get("/", (req, res) => {
+    return res.json({ success: true, message: "Successfully deployed." })
+})
+
+app.use("/", Router);
 app.use("/api/user", UserRoutes);
 app.use("/api/skill", SkillRoutes);
 
