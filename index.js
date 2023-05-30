6279c92b9d58a02f0b987d7af5d1dcc44e80845a
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-30 22:30:20 +0530

    vercel.json change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
     cors({
-        origin: "*",
+        origin: ['https://markovate-react-app.vercel.app', "http://localhost:3000"],
     })
 );
 
