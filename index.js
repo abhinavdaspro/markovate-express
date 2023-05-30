419b84ed55e58f34a4233f9c4e89f583947ac8ea
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-30 22:43:23 +0530

    cors policy change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
     cors({
-        origin: ['https://markovate-react-app.vercel.app/api/*', "http://localhost:3000"],
+        origin: '*'
     })
 );
 
