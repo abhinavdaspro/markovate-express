e591662a15e3f8ef3ffe1c45f50dbce06ee563fa
Abhinav Das <abhinavdaspro@gmail.com>
2023-05-31 11:33:27 +0530

    change in cors

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const Router = express.Router();
+const DbConnection = require("./DBConnection/connection");
+const UserRoutes = require("./routes/userRoutes")
+const SkillRoutes = require("./routes/SkillRoute");
+var timeout = require('connect-timeout')
+const PORT = 8000;
+const app = express();
+app.use(cors())
+// app.use(cors({
+//     origin: "*",
+//     optionsSuccessStatus: 200,
+//     methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+// app.use(timeout('30s'));
 
-const app = express()
-const PORT = 8000
+
+// app.use(express.urlencoded({ extended: false }));
+
+// app.options('*', cors())
+// app.use(cors({
+//     origin: ['https://markovate-react-app.vercel.app', "http://localhost:3000"]
+// }));
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.use(express.json());
+
+Router.get("/", (req, res) => {
+    return res.json({ success: true, message: "Successfully deployed." })
+})
+
+app.use("/", Router);
+app.use("/api/user", cors(), UserRoutes);
+app.use("/api/skill", SkillRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `)
-})
-
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-    res.send('This is my about route..... ')
-})
-app.post('/api/user/register', (req, res) => {
-    res.send('Post API..... ')
+    console.log("Server started at " + PORT);
+    DbConnection();
 })
